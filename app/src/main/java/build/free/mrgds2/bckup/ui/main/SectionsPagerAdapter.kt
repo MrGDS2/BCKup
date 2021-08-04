127dbf0940c3d2c0413017e39e489282b5beb893
MrGDS2 <mrsonicthunder@gmail.com>
2021-08-04 12:14:22 -0400

    home page layout set

--- app/src/main/java/build/free/mrgds2/bckup/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/build/free/mrgds2/bckup/ui/main/SectionsPagerAdapter.kt
@@ -1,14 +1,16 @@
 package build.free.mrgds2.bckup.ui.main
 
 import android.content.Context
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import build.free.mrgds2.bckup.R
+import build.free.mrgds2.bckup.*
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -21,7 +23,21 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+//        return PlaceholderFragment.newInstance(position + 1)
+
+
+        var fragment: Fragment = when (position) {
+            0 -> RecordFragment()
+
+            1 -> ContactsFragment()
+
+            2 -> TrackerFragment()
+
+            else ->
+                RecordFragment()
+        }
+
+        return fragment
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -29,7 +45,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }
