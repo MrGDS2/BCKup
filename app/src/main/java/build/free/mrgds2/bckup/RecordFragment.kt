7060d10e6541757d7f8a42b2c333b6c5d3b29ef5
MrGDS2 <mrsonicthunder@gmail.com>
2021-08-09 11:00:23 -0400

    layout UI for Record Fragment

--- app/src/main/java/build/free/mrgds2/bckup/RecordFragment.kt
+++ app/src/main/java/build/free/mrgds2/bckup/RecordFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +27,7 @@ class RecordFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
+    lateinit var  record_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +35,11 @@ class RecordFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
+         // init rating stars
+
+
     }
 
     override fun onCreateView(
