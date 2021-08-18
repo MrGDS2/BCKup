bcd9587120e5f090a0debab5c04060d0317dea15
MrGDS2 <mrsonicthunder@gmail.com>
2021-08-18 13:20:26 -0400

    check record error

--- app/src/main/java/build/free/mrgds2/bckup/RecordFragment.kt
+++ app/src/main/java/build/free/mrgds2/bckup/RecordFragment.kt
@@ -1,6 +1,8 @@
 package build.free.mrgds2.bckup
 
 
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -10,10 +12,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat.getDrawable
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import java.io.IOException
+import java.util.jar.Manifest
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -30,12 +36,18 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RecordFragment : Fragment() {
-    lateinit var  ibRecord_button: ImageButton
+    lateinit var ibRecord_button: ImageButton
     lateinit var ibRecordListBtn: ImageButton
-    lateinit var navController : NavController
-    private  var isRecording: Boolean=false
+    private var isRecording: Boolean = false
+    private val TAG = "RecordPermission"
+    private val RECORD_REQUEST_CODE = 101
+    private val mediaRecorder: MediaRecorder? = null
+    private var mediaPath: String = ""
+    private var mediaFile: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -47,32 +59,26 @@ class RecordFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_record, container, false)
 
-        ibRecord_button= view.findViewById(R.id.record_btn)
-        ibRecordListBtn= view.findViewById(R.id.record_list_btn)
+        ibRecord_button = view.findViewById(R.id.record_btn)
+        ibRecordListBtn = view.findViewById(R.id.record_list_btn)
 
 
         ibRecord_button.setOnClickListener {
-            Toast.makeText(activity, "Recording now..", Toast.LENGTH_SHORT).show()
 
-            isRecording = if (isRecording){
-                //stop recording
-                ibRecord_button.setImageDrawable(resources.getDrawable(R.drawable.record_btn_stopped,null))
-                false
-            } else {
-                //start recording
-                ibRecord_button.setImageDrawable(resources.getDrawable(R.drawable.record_btn_recording,null))
-                true
-            }
+            if (setupPermissions()) {
+                toggleButtonRecord()
+            } else
+                Toast.makeText(activity, "Permission Denied", Toast.LENGTH_SHORT).show()
         }
 
         ibRecordListBtn.setOnClickListener {
             Toast.makeText(activity, "Showing list!", Toast.LENGTH_SHORT).show()
-           Navigation.findNavController(it).navigate(R.id.action_recordFragment_to_audioFragment)
+            Navigation.findNavController(it).navigate(R.id.action_recordFragment_to_audioFragment)
         }
 
 
         // Inflate the layout for this fragment
-           return view
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -84,15 +90,87 @@ class RecordFragment : Fragment() {
         super.onDetach()
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
+    private fun setupPermissions(): Boolean {
+        val permission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.RECORD_AUDIO
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied")
+            makeRequest()
+            return false
+        }
+        return true
+    }
+
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(android.Manifest.permission.RECORD_AUDIO),
+            RECORD_REQUEST_CODE
+        )
+    }
+
+
+    private fun toggleButtonRecord() {
+        isRecording = if (isRecording) {
+            //stop recording
+            stopRecording()
+            ibRecord_button.setImageDrawable(
+                resources.getDrawable(
+                    R.drawable.record_btn_stopped,
+                    null
+                )
+            )
+            false
+        } else {
+            //start recording
+            startRecording()
+            ibRecord_button.setImageDrawable(
+                resources.getDrawable(
+                    R.drawable.record_btn_recording,
+                    null
+                )
+            )
+            true
+
+
+        }
+    }
+
+    private fun startRecording() {
+
+        Toast.makeText(activity, "Recording now..", Toast.LENGTH_SHORT).show()
+
+        //path where file is saved after recording
+        mediaPath = requireActivity().getExternalFilesDir("/")!!.absolutePath
+        mediaFile = "BckUP_File_20210818.mp3"
+
+        //TODO:ERROR CHECK
+        mediaRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecorder.setOutputFile("$mediaPath/$mediaFile")
+        //TODO: try catch?
+        try {
+            mediaRecorder.prepare()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mediaRecorder.start()
+    }
+
+
+    private fun stopRecording() {
+
+        val savedFileName = "$mediaPath/$mediaFile"
+
+        Toast.makeText(activity, "Saved file: /$savedFileName", Toast.LENGTH_SHORT).show()
+        mediaRecorder!!.stop()
+        mediaRecorder.release()
+    }
+
 }
